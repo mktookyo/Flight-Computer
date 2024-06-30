5432f83a848f43f51db9986c8e6f9e8b3def63cc
mktookyo <mktookyoo@gmail.com>
2024-06-30 18:55:02 +0600

    Functionality
    Added checking if BMP280 started.
    variometer renamed to vertical_speed (returns vertical speed)
    Changed baud speed of Serial port for ESP32
    Added some comments

--- Code/flight_computer_v1.0/flight_computer_v1.0.ino
+++ Code/flight_computer_v1.0/flight_computer_v1.0.ino
@@ -3,16 +3,11 @@ Arduino UNO/Nano
 NRF24L01: CE-7 CSN-8 SCK-13 MOSI-11 MISO-12 VCC-3.3V
 Buzzer-Digital pin(+) GND(-)
 BMP280: SCL-A5 SDA-A4 VCC-5V
+NeoGPS 6m: Rx-Rx Tx-Tx Vcc-5/3.3V
 */
-
-
-
-
-
-
-
-
-
+/* ESP32
+BMP280: SCL-D22 SDA-D21 VCC-3.3V/5V
+*/
 
 // Imports
 #include <GyverBME280.h>    //BMP280 Library
@@ -23,11 +18,17 @@ GyverBME280 bme;    //Crating object bme for BMP280
 
 // Variables
 float humidity, temp, pressure;
+
+// Setup
 void setup() {
-    Serial.begin(9600);
-    FILTER_COEF_4;
-    bme.begin(); 
+  j
+  Serial.begin(115200); //Activating serial port (115200 baud) for ESP32
+  if (!bme.begin(0x76)) {  // Check if BMP280 connected
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    while (1);
+  }
 }
+// just prints temperature, Humidity, Pressure.
 void weatherData(){
   Serial.print(bme.readTemperature());
   Serial.println(" Celsius");
@@ -36,35 +37,22 @@ void weatherData(){
   Serial.print(bme.readPressure());
   Serial.println(" Pa");
 }
-void variometer(){
-  float alt1, alt2, dalt, acceleration;
-  alt1=round(pressureToAltitude(bme.readPressure()));
-  // double 
-  // Serial.println(alt1);
-  delay(200);
-  alt2=roundf(pressureToAltitude(bme.readPressure()));
-  // Serial.println(alt2);
-  dalt=alt2-alt1;
-  acceleration=dalt/0.2;
-  if(dalt!=0){
-    Serial.println(dalt);
-    Serial.println(acceleration);
-  }
-  Serial.println("//////////////////////////////////////////////////////////");
+// vertical speed function
+float verticalSpeed(){
+  // version 1.0 (idk about accuracy but it seems to work) дальше в будущих версиях проверка точности показания скорости с помощью акселлерометра (наличие ускорения)
+  float alt1, alt2, dalt, vertical_speed;
+  alt1=rounding(pressureToAltitude(bme.readPressure()) , 2 ); //Reading altitude #1 
+  delay(200); //Waiting 200ms 
+  alt2=rounding(pressureToAltitude(bme.readPressure()) , 2 ); //Reading altitude #2
+  dalt=alt2-alt1; //difference between altitudes 2 and 1
+  vertical_speed=dalt/0.2; //calculating speed gained in 200ms
+  return vertical_speed;
 }
+
 double rounding(double num, int decimal_places) { //функция округления значений с несколькими знаками после запятой. rounding(число, знаков после запятой)
-  double multiplier = pow(10, decimal_places);
+  double multiplier = pow(10, decimal_places); 
   return round(num * multiplier) / multiplier; //возвращает округленное число 
 }
 void loop() {
-  // variometer();
-  Serial.print(bme.readTemperature());
-  Serial.println(" Celsius");
-  Serial.print(bme.readHumidity());
-  Serial.println("% Humidity");
-  Serial.print(bme.readPressure());
-  Serial.println(" Pa");
-  Serial.println(pressureToAltitude(bme.readPressure()));
-  // Serial.println(pressureToAltitude(101325));
-  delay(1000);
+
 }
