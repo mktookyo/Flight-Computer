4d11a33cc3adddb00988f08815211068da60b1e7
mktookyo <mktookyoo@gmail.com>
2024-07-04 00:58:18 +0600

    GPS integration

--- Code/flight_computer_v1.0/flight_computer_v1.0.ino
+++ Code/flight_computer_v1.0/flight_computer_v1.0.ino
@@ -7,52 +7,104 @@ NeoGPS 6m: Rx-Rx Tx-Tx Vcc-5/3.3V
 */
 /* ESP32
 BMP280: SCL-D22 SDA-D21 VCC-3.3V/5V
+NeoGPS 6m: TX-RXD2 RX-TXD2 vcc-3.3/5v
 */
 
-// Imports
-#include <GyverBME280.h>    //BMP280 Library
-#include <math.h>
+// libraries
+#include <Wire.h>
+#include <TinyGPS++.h> //GPS neo-6m
+#include <GyverBME280.h>    //BMP280 
+// #include <math.h> //maybe in future ;-;
 // #include <preferences.h>
-// Objects
-GyverBME280 bme;    //Crating object bme for BMP280
 
 // Variables
-float humidity, temp, pressure;
+struct weatherstruct {
+  float humidity, temp, pressure;
+};
+// defining pin numbers
+#define RXD2 16
+#define TXD2 17
 
-// Setup
-void setup() {
-  j
-  Serial.begin(115200); //Activating serial port (115200 baud) for ESP32
-  if (!bme.begin(0x76)) {  // Check if BMP280 connected
-    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
-    while (1);
-  }
-}
+// Objects
+GyverBME280 bme;    //Crating object bme for BMP280  
+weatherstruct weather; //object for weather variables (ex: weather.humidity=....) 
+HardwareSerial neogps(1); //creating serial port for gps
+TinyGPSPlus gps; //object for gps module 
+
 // just prints temperature, Humidity, Pressure.
 void weatherData(){
-  Serial.print(bme.readTemperature());
+  weather.humidity=bme.readHumidity(); //reading humidity from BMP280
+  weather.temp=bme.readTemperature(); //reading temperature from BMP280
+  weather.pressure=bme.readPressure(); //reading pressure from BMP280
+  Serial.print(weather.temp);
   Serial.println(" Celsius");
-  Serial.print(bme.readHumidity());
+  Serial.print(weather.humidity);
   Serial.println("% Humidity");
-  Serial.print(bme.readPressure());
+  Serial.print(weather.pressure);
   Serial.println(" Pa");
 }
-// vertical speed function
+// function returns vertical speed in float format
 float verticalSpeed(){
-  // version 1.0 (idk about accuracy but it seems to work) дальше в будущих версиях проверка точности показания скорости с помощью акселлерометра (наличие ускорения)
+  // version 1.0  дальше в будущих версиях проверка точности показания скорости с помощью акселлерометра (наличие ускорения)
   float alt1, alt2, dalt, vertical_speed;
-  alt1=rounding(pressureToAltitude(bme.readPressure()) , 2 ); //Reading altitude #1 
+  alt1=rounding(pressureToAltitude(bme.readPressure()) , 3 ); //Reading altitude #1 
   delay(200); //Waiting 200ms 
-  alt2=rounding(pressureToAltitude(bme.readPressure()) , 2 ); //Reading altitude #2
+  alt2=rounding(pressureToAltitude(bme.readPressure()) , 3 ); //Reading altitude #2
   dalt=alt2-alt1; //difference between altitudes 2 and 1
   vertical_speed=dalt/0.2; //calculating speed gained in 200ms
-  return vertical_speed;
+  return vertical_speed; 
 }
 
 double rounding(double num, int decimal_places) { //функция округления значений с несколькими знаками после запятой. rounding(число, знаков после запятой)
   double multiplier = pow(10, decimal_places); 
   return round(num * multiplier) / multiplier; //возвращает округленное число 
 }
+void variometer(){
+  
+}
+boolean gpsChecker(){ //функция возвращает true если до модуля gps можно дозвониться
+  boolean newData = false;
+  for (unsigned long start = millis(); millis() - start < 1000;)
+  {
+    while (neogps.available())
+    {
+      if (gps.encode(neogps.read()))
+      {
+        newData = true;
+      }
+    }
+  }
+  return newData;
+}
+void gpsFastRead(){
+  Serial.print("Latitude: ");
+  Serial.println(gps.location.lat());
+  Serial.print("Lontitude: ");
+  Serial.println(gps.location.lng());
+  Serial.print("Altitude: ");
+  Serial.println(gps.altitude.meters());
+  Serial.print("Satelites: ");
+  Serial.println(gps.satellites.value());
+  Serial.print("Speed: ");
+  Serial.println(gps.speed.kmph());
+}
+// Setup
+void setup() {
+  Serial.begin(115200); //Activating serial port (115200 baud) for ESP32
+  if (!bme.begin(0x76)) {  // Check if BMP280 connected
+    Serial.println("Could not find a valid BMP280 sensor");
+    // while (1);
+  }
+  Serial.println("bmp280 has started");
+  neogps.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  if (gpsChecker()!=true){
+    Serial.println("Couldn't find GPS module");
+    while(1);
+  }
+  Serial.println("GPS has started!");
+}
 void loop() {
-
+  byte data = neogps.read();
+  Serial.write(data);
+  // delay(500);
 }
--- Some reference/GPS_example_code/GPS_example_code.ino
+++ Some reference/GPS_example_code/GPS_example_code.ino
@@ -1,7 +1,7 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 
-static const int RXPin =3, TXPin = 4;
+static const int RXPin =3, TXPin = 1;
 static const uint32_t GPSBaud = 9600;
 
 // The TinyGPS++ object
